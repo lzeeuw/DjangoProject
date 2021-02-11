365a6fb3bb83b4f5aa5b1c452955b7608d426ccc
Lucas ZEEUW <lzeeuw@aubay.com>
2021-02-11 16:53:47 +0100

    Initial commit

---
+++ djangoproject01/urls.py
@@ -0,0 +1,45 @@
+"""ChatServerPlayground URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.generic import RedirectView
+from django.conf.urls import url
+
+from personal.views import(
+    home_screen_view,
+)
+
+from account.views import(
+    register_view,
+    login_view,
+    logout_view,
+)
+
+urlpatterns = [
+    path('', home_screen_view, name='home'),
+    path('admin/', admin.site.urls),    
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name="register"),
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico'))
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
